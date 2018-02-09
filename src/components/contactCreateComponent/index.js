48b9a77a251f05a9f922566431f8e99f3abcc269
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2018-02-09 15:19:13 +0200

    add real time validation with state

--- src/components/contactCreateComponent/index.js
+++ src/components/contactCreateComponent/index.js
@@ -8,33 +8,38 @@ export default class ContactCreate extends Component {
     super(props);
 
     this.state = {
-      user: this.user = {
-        name: '',
-        email: '',
-        phone: '',
-        address: '',
-        postcode: '',
-        date: '',
-        formValid: true
-      },
+
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+      postcode: '',
+      date: '',
+      formValid: true,
+
       nameValid: false,
       emailValid: false,
       postcodeValid: false,
       dateValid: false,
-      formValid: true
+
+      errorsName: '',
+      errorsEmail: '',
+      errorsPostcode: '',
+      errorsDate: '',
+
     };
 
 
-    this.changeName = this.changeName.bind(this)
-    this.changeEmail = this.changeEmail.bind(this)
-    this.changePhone = this.changePhone.bind(this)
-    this.changePostcode = this.changePostcode.bind(this)
-    this.changeAddress = this.changeAddress.bind(this)
-    this.changeDate = this.changeDate.bind(this)
-    this.updateState = this.updateState.bind(this)
+    this.changeName = this.changeName.bind(this);
+    this.changeEmail = this.changeEmail.bind(this);
+    this.changePhone = this.changePhone.bind(this);
+    this.changePostcode = this.changePostcode.bind(this);
+    this.changeAddress = this.changeAddress.bind(this);
+    this.changeDate = this.changeDate.bind(this);
+    this.updateState = this.updateState.bind(this);
 
     // this.clearState = this.clearState.bind(this)
-    this.addPlate = this.addPlate.bind(this)
+
   }
 
   // clearState() {
@@ -61,129 +66,102 @@ export default class ContactCreate extends Component {
   // }
 
 
-  addPlate(e) {
-
-
-    if (this.state.nameValid !== false && this.state.nameValid !== undefined
-        && this.state.emailValid !== false && this.state.emailValid !== undefined
-        && this.state.postcodeValid !== false && this.state.postcodeValid !== undefined
-        && this.state.dateValid !== false && this.state.dateValid !== undefined){
-
-
-
-      if (this.state.emailValid === true) {
-        this.props.changeStateProps('email', this.state.email);
-      } else {
-        document.querySelector('.input__email').classList.add('input__error')
-      }
-
-      this.props.changeStateProps('phone', this.state.phone);
-      this.props.changeStateProps('address', this.state.address);
-
-      if (this.state.postcodeValid === true) {
-        this.props.changeStateProps('postcode', this.state.postcode);
-      } else {
-        document.querySelector('.input__postcode').classList.add('input__error')
-      }
-
-      if (this.state.dateValid === true) {
-        this.props.changeStateProps('date', this.state.date);
-      } else {
-        document.querySelector('.input__date').classList.add('input__error')
-      }
-
-      this.clearState()
-
-    } else {
-
-      if (this.state.nameValid === false || this.state.nameValid === undefined) {
-        document.querySelector('.input__name').classList.add('input__error')
-      }
-
-      if (this.state.emailValid === false || this.state.emailValid === undefined) {
-        document.querySelector('.input__email').classList.add('input__error')
-      }
-
-      if (this.state.postcodeValid === false || this.state.postcodeValid === undefined) {
-        document.querySelector('.input__postcode').classList.add('input__error')
-      }
-      if (this.state.dateValid === false || this.state.dateValid === undefined) {
-        document.querySelector('.input__date').classList.add('input__error')
-      }
-
-
-    }
-  }
-
   componentDidMount() {
-    console.log('props in component', this.props.user)
+    console.log('props in component', this.props)
   }
 
   changeName(event) {
 
-    this.setState({user: { name: event.target.value}});
+    this.setState({name: event.target.value});
 
     if (event.target.value.length > 2) {
-      this.setState({nameValid: true});
+      this.setState({
+        nameValid: true,
+        errors: {name: ''}
+      });
+
     } else {
-      this.setState({nameValid: false});
+      this.setState({
+        nameValid: false,
+        errorsName: 'Your name should be more long'
+      });
     }
   }
 
 
   changeEmail(event) {
-    this.setState({user: { email: event.target.value}});
+    this.setState({email: event.target.value});
 
     if (event.target.value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i) !== null) {
-      this.setState({emailValid: true});
+      this.setState({
+        emailValid: true,
+        errorsEmail: ''
+      });
     } else {
-      this.setState({emailValid: false});
+      this.setState({
+        emailValid: false,
+        errorsEmail: 'This is not email'
+      });
+
     }
 
   }
 
   changePhone(event) {
     if (event.target.value.match(/^[0-9]+$/) !== null) {
-      this.setState({user: { phone: event.target.value}});
+      this.setState({phone: event.target.value});
     }
   }
 
   changeAddress(event) {
-    this.setState({user: { address: event.target.value}});
+    this.setState({address: event.target.value});
   }
 
   changePostcode(event) {
 
-    this.setState({user: { postcode: event.target.value}});
+    this.setState({postcode: event.target.value});
 
     if (event.target.value.length >= 5 && event.target.value.length < 10) {
 
-      this.setState({postcodeValid: true});
+      this.setState({
+        postcodeValid: true,
+        errorsPostcode: ''
+      });
     } else {
-       this.setState({postcodeValid: false});
+      this.setState({
+        postcodeValid: false,
+        errorsPostcode: 'Postcode should be from 5 to 10 symbols'
+      });
     }
   }
 
   changeDate(event) {
-    this.setState({user: { date: event.target.value}});
+    this.setState({date: event.target.value});
 
     if (event.target.value.match(/(^\d{1,4}[\.|\\/|-]\d{1,2}[\.|\\/|-]\d{1,4})(\s*(?:0?[1-9]:[0-5]|1(?=[012])\d:[0-5])\d\s*[ap]m)?$/) !== null) {
 
-      this.setState({dateValid: true});
+      this.setState({
+        dateValid: true,
+        errorsDate:''
+      });
     } else {
-       this.setState({dateValid: false});
+      this.setState({
+        dateValid: false,
+        errorsDate: 'Not valid format of Date'
+      });
     }
   }
+
   updateState() {
 
-    console.log(this.state.user);
+    console.log(this.state);
     if (this.state.formValid &&
         this.state.nameValid && this.state.nameValid !== undefined
         && this.state.emailValid && this.state.emailValid !== undefined
-        && this.state.postcodeValid  && this.state.postcodeValid !== undefined
+        && this.state.postcodeValid && this.state.postcodeValid !== undefined
         && this.state.dateValid && this.state.dateValid !== undefined) {
 
-        this.props.changeStateProps('user', this.state.user)
+      this.props.changeStateProps('user', this.state)
 
     }
 
@@ -206,8 +184,9 @@ export default class ContactCreate extends Component {
                 type='text'
                 onChange={this.changeName}
                 placeholder='Name'
-                value={this.state.user.name}
+                value={this.state.name}
             />
+            <p className="input__errors-info"> {this.state.errorsName}</p>
           </label>
 
           <label className='input__label'>Email :
@@ -216,9 +195,10 @@ export default class ContactCreate extends Component {
                 type='text'
                 placeholder='Email'
                 onChange={this.changeEmail}
-                value={this.state.user.email}
+                value={this.state.email}
             />
-          </label>
+            <p className="input__errors-info">{this.state.errorsEmail}</p>
+        </label>
           <label className='input__label'>Phone :
             <input
                 className='input__phone input__style'
@@ -226,7 +206,7 @@ export default class ContactCreate extends Component {
                 name='phone'
                 placeholder='Phone'
                 onChange={this.changePhone}
-                value={this.state.user.phone}
+                value={this.state.phone}
             />
           </label>
           <label className='input__label'>address :
@@ -236,7 +216,7 @@ export default class ContactCreate extends Component {
                 name="address "
                 placeholder='address '
                 onChange={this.changeAddress}
-                value={this.state.user.address}
+                value={this.state.address}
             />
           </label>
           <label className='input__label'>Postcode :
@@ -246,8 +226,9 @@ export default class ContactCreate extends Component {
                 name="postcode "
                 placeholder='postcode  '
                 onChange={this.changePostcode}
-                value={this.state.user.postcode}
+                value={this.state.postcode}
             />
+            <p className="input__errors-info"> {this.state.errorsPostcode}</p>
           </label>
           <label className='input__label'>date of birth :
             <input
@@ -256,15 +237,10 @@ export default class ContactCreate extends Component {
                 name="date_of_birth "
                 placeholder='date of birth '
                 onChange={this.changeDate}
-                value={this.state.user.date}
+                value={this.state.date}
             />
+            <p className="input__errors-info">  {this.state.errorsDate}</p>
           </label>
-          <div className="plate__footer">
-            <div className="plate__footer-error">
-              tis my error
-
-            </div>
-          </div>
 
         </form>
     )
